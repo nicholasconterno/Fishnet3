2feefcf95acda28176c2d1014b0915aa831a4608
nicholasconterno <nec21@duke.edu>
2024-02-06 17:06:06 -0500

    added is_fish and created a set of images for our dataset

--- data-preprocessing/label_parser.py
+++ data-preprocessing/label_parser.py
@@ -1,7 +1,6 @@
 import csv
 
-
-# hashmap format: {img_id: [((x_min, x_max), (y_min, y_max), label), ...]}
+# hashmap format: {img_id: [is_fish, [((x_min, x_max), (y_min, y_max), label), ...]]}
 def process_data(file_path):
     data_map = {}
 
@@ -14,24 +13,19 @@ def process_data(file_path):
             y_min = row['y_min']
             y_max = row['y_max']
             label = row['label_l1']
+            is_fish = row['is_fish'].lower()=='true' # Convert to boolean
 
             # Create the tuple for this row
             data_tuple = ((x_min, x_max), (y_min, y_max), label)
 
             # Check if the img_id is already a key in the dictionary
             if img_id in data_map:
-                data_map[img_id].append(data_tuple)
+                data_map[img_id][1].append(data_tuple)
+                # Update is_fish if necessary
+                if is_fish:
+                    data_map[img_id][0] = True
             else:
-                data_map[img_id] = [data_tuple]
+                # Initialize the entry with is_fish and the list of data tuples
+                data_map[img_id] = [is_fish, [data_tuple]]
 
     return data_map
-
-# Use the function with the path to your CSV file
-data = process_data('data/fishnet_labels.csv')
-c=0
-for i in data.keys():
-    print(i, data[i])
-    c+=1
-    if c==5:
-        break
-print(len(data))
